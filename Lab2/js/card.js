6279581356c69c5022131e3153e1be47e469db78
GilianS <r0885351@student.thomasmore.be>
2022-10-02 00:25:13 +0200

    Added marking cards as done

--- Lab2/js/card.js
+++ Lab2/js/card.js
@@ -4,21 +4,23 @@ export default class Card {
   constructor(title) {
     // the constructor is called when you create a new instance of the class
     this.title = title;
-    console.log(`Created a new card with title: ${title}`);
+    //console.log(`Created a new card with title: ${title}`);
   }
-
+  /**
+   * 
+   * @param {Element} target 
+   */
   markDone(target) {
     // to mark a card as done, we add a class .bingo__card--done to it
     // 🔥🔥🔥 TODO 5: mark or unmark (toggle) a bingo card when clicked
     console.log("Marking card as done");
-    console.log(target);
-    // hint: use class .bingo__card--done
+    target.classList.add('bingo__card--done');
   }
 
   render(counter) {
     // rendering the card to the screen is done by building up a string of HTML
     // after that, we append the HTML to the DOM - check the index.html file to see what structure to use
-    console.log("Rendering card...");
+    //console.log("Rendering card...");
 
     // 🔥🔥🔥 TODO3: build the HTML element and append it to the DOM
     let card = document.createElement("div");
@@ -26,14 +28,14 @@ export default class Card {
     card.classList.add("bingo__card");
     card.dataset.number = counter + 1;
     let board = document.querySelector(".bingo__board");
-    console.log({board, card});
+    //console.log({board, card});
     board.appendChild(card);
 
     // 🔥🔥🔥 TODO4: when we click an item, we want to check for winners and we want to save the selection to storage
     card.addEventListener("click", (e) => {
-      // this.markDone(e.target);
-      // call checkWinner() on the Bingo class
-      // try to call the save() method on the Bingo class
+      this.markDone(e.target);
+      Bingo.checkWinner();
+      Bingo.save();
     });
   }
 }
