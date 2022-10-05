96b0ce8c20a173e7aa89697ccb08f2197ea50c2b
GilianS <r0885351@student.thomasmore.be>
2022-10-05 23:20:26 +0200

    feat: fetch weather from openWeatherAPI

--- Lab3/src/ad.js
+++ Lab3/src/ad.js
@@ -1,15 +1,20 @@
+import WeatherAPI from "./weather";
 const WEATHER_URI = "https://api.openweathermap.org/data/2.5/weather?";
 
 export default class Ad {
     constructor(apiKey) {
         this.apiKey = apiKey;
+        this.weatherAPI = new WeatherAPI(apiKey);
 
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                this.getWeather.bind(this)
-            );
+            navigator.geolocation.getCurrentPosition(this.getWeather.bind(this)); 
         } else {
             alert("This browser does not support geolocation");
         }
+    }
+
+    async getWeather(position) {
+        const weather = await this.weatherAPI.getWeather(position);
+        console.log(weather);
     }
 }
--- Lab3/src/weather.js
+++ Lab3/src/weather.js
@@ -6,7 +6,7 @@ export default class WeatherAPI {
     getWeather(position) {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=imperial`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`;
 
         return fetch(url)
             .then(response => response.json())
