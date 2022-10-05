aad4dde5ce574dfc91410851c4b576076c4df4e4
GilianS <r0885351@student.thomasmore.be>
2022-10-05 23:12:58 +0200

    feat: Added class for weather API

---
+++ Lab3/src/weather.js
@@ -0,0 +1,20 @@
+export default class WeatherAPI {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    getWeather(position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=imperial`;
+
+        return fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const weather = data.weather[0].main;
+                const temp = data.main.temp;
+
+                return { weather, temp };
+            });
+    }
+}
