7139f92a2b8818db890721330aa5bcc4a4e48cf4
GilianS <r0885351@student.thomasmore.be>
2022-10-13 00:28:54 +0200

    feat: Added texture

--- Lab4/src/house.js
+++ Lab4/src/house.js
@@ -4,23 +4,61 @@ export default class House {
         this.scene = scene;
         this.createStructure();
     }
-    
+
     textureLoader = new THREE.TextureLoader();
 
-    // Textures
-    tBricks = this.textureLoader.load('../textures/stonewall/Stone_Wall_007_COLOR.jpg');
-    nmBricks = this.textureLoader.load('../textures/stonewall/Stone_Wall_007_NRM.jpg');
-    
+    // Brick textures
+    tBricks = this.textureLoader.load(
+        "../textures/stonewall/Stone_Wall_007_COLOR.jpg"
+    );
+    nBricks = this.textureLoader.load(
+        "../textures/stonewall/Stone_Wall_007_NORM.jpg"
+    );
+    rBricks = this.textureLoader.load(
+        "../textures/stonewall/Stone_Wall_007_ROUGH.jpg"
+    );
+    aoBricks = this.textureLoader.load(
+        "../textures/stonewall/Stone_Wall_007_OCC.jpg"
+    );
+
+    // Roof textures
+    tRoof = this.textureLoader.load(
+        "../textures/roof/Roof_Tiles_Terracota_002_basecolor.jpg"
+    );
+    nRoof = this.textureLoader.load(
+        "../textures/roof/Roof_Tiles_Terracota_002_normal.jpg"
+    );
+    rRoof = this.textureLoader.load(
+        "../textures/roof/Roof_Tiles_Terracota_002_roughness.jpg"
+    );
+    aoRoof = this.textureLoader.load(
+        "../textures/roof/Roof_Tiles_Terracota_002_ambientOcclusion.jpg"
+    );
+
     // Brick material texture
-    mBricks = new THREE.MeshLambertMaterial({ color: 0xffffff, map: this.tBricks, lightMap: this.nmBricks });
-    mRoof = new THREE.MeshLambertMaterial({ color: 0x993333 });
-    mDoor = new THREE.MeshLambertMaterial({ color: 0x332211 });
+    mBricks = new THREE.MeshLambertMaterial({
+        color: 0xffffff,
+        map: this.tBricks,
+        normalMap: this.nBricks,
+        roughnessMap: this.rBricks,
+        aoMap: this.aoBricks,
+    });
+    mRoof = new THREE.MeshLambertMaterial({
+        color: 0xfff,
+        map: this.tRoof,
+        normalMap: this.nRoof,
+        roughnessMap: this.rRoof,
+        aoMap: this.aoRoof,
+    });
+    mDoor = new THREE.MeshLambertMaterial({
+        color: 0xfff,
+    });
 
     createStructure() {
         let geometry = new THREE.BoxGeometry(8, 4, 8);
         let material = this.mBricks;
         let cube = new THREE.Mesh(geometry, material);
         this.scene.add(cube);
-        console.log('createStructure');
+        console.log("createStructure");
     }
 }
