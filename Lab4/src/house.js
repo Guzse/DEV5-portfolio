7d743ebdc8e5220886bd35bb04ebc7ea20cb0ea9
GilianS <r0885351@student.thomasmore.be>
2022-10-13 00:33:02 +0200

    refactor: moved textures to seperate class

--- Lab4/src/house.js
+++ Lab4/src/house.js
@@ -1,54 +1,27 @@
 import * as THREE from 'three';
+import Textures from './textures';
 export default class House {
     constructor(scene) {
         this.scene = scene;
         this.createStructure();
     }
 
-    textureLoader = new THREE.TextureLoader();
-
-    // Brick textures
-    tBricks = this.textureLoader.load(
-        "../textures/stonewall/Stone_Wall_007_COLOR.jpg"
-    );
-    nBricks = this.textureLoader.load(
-        "../textures/stonewall/Stone_Wall_007_NORM.jpg"
-    );
-    rBricks = this.textureLoader.load(
-        "../textures/stonewall/Stone_Wall_007_ROUGH.jpg"
-    );
-    aoBricks = this.textureLoader.load(
-        "../textures/stonewall/Stone_Wall_007_OCC.jpg"
-    );
-
-    // Roof textures
-    tRoof = this.textureLoader.load(
-        "../textures/roof/Roof_Tiles_Terracota_002_basecolor.jpg"
-    );
-    nRoof = this.textureLoader.load(
-        "../textures/roof/Roof_Tiles_Terracota_002_normal.jpg"
-    );
-    rRoof = this.textureLoader.load(
-        "../textures/roof/Roof_Tiles_Terracota_002_roughness.jpg"
-    );
-    aoRoof = this.textureLoader.load(
-        "../textures/roof/Roof_Tiles_Terracota_002_ambientOcclusion.jpg"
-    );
+    textures = new Textures();
 
     // Brick material texture
     mBricks = new THREE.MeshLambertMaterial({
         color: 0xffffff,
-        map: this.tBricks,
-        normalMap: this.nBricks,
-        roughnessMap: this.rBricks,
-        aoMap: this.aoBricks,
+        map: this.textures.tBricks,
+        normalMap: this.textures.nBricks,
+        roughnessMap: this.textures.rBricks,
+        aoMap: this.textures.aoBricks,
     });
     mRoof = new THREE.MeshLambertMaterial({
         color: 0xfff,
-        map: this.tRoof,
-        normalMap: this.nRoof,
-        roughnessMap: this.rRoof,
-        aoMap: this.aoRoof,
+        map: this.textures.tRoof,
+        normalMap: this.textures.nRoof,
+        roughnessMap: this.textures.rRoof,
+        aoMap: this.textures.aoRoof,
     });
     mDoor = new THREE.MeshLambertMaterial({
         color: 0xfff,
---
+++ Lab4/src/textures.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three';
+export default class Textures {
+    textureLoader = new THREE.TextureLoader();
+    
+    // Brick textures
+    tBricks = this.textureLoader.load(
+        "../textures/stonewall/Stone_Wall_007_COLOR.jpg"
+    );
+    nBricks = this.textureLoader.load(
+        "../textures/stonewall/Stone_Wall_007_NORM.jpg"
+    );
+    rBricks = this.textureLoader.load(
+        "../textures/stonewall/Stone_Wall_007_ROUGH.jpg"
+    );
+    aoBricks = this.textureLoader.load(
+        "../textures/stonewall/Stone_Wall_007_OCC.jpg"
+    );
+
+    // Roof textures
+    tRoof = this.textureLoader.load(
+        "../textures/roof/Roof_Tiles_Terracota_002_basecolor.jpg"
+    );
+    nRoof = this.textureLoader.load(
+        "../textures/roof/Roof_Tiles_Terracota_002_normal.jpg"
+    );
+    rRoof = this.textureLoader.load(
+        "../textures/roof/Roof_Tiles_Terracota_002_roughness.jpg"
+    );
+    aoRoof = this.textureLoader.load(
+        "../textures/roof/Roof_Tiles_Terracota_002_ambientOcclusion.jpg"
+    );
+}
