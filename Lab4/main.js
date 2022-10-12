bf914ac0c102f91cc1695b9e0362d038d53588ab
GilianS <r0885351@student.thomasmore.be>
2022-10-13 00:02:02 +0200

    refactor: created light class

--- Lab4/main.js
+++ Lab4/main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import * as THREE from 'three';
 import { WebGL1Renderer } from 'three';
+import Light from './src/light';
 import House from './src/house';
 import Terrain from './src/terrain';
 import Camera from './src/camera';
@@ -15,35 +16,14 @@ let camera = new Camera(scene, renderer);
 
 let house = new House(scene);
 let terrain = new Terrain(scene);
-
-// Add ambient light
-const ambientLight = new THREE.AmbientLight(0xccccff, 0.2);
-scene.add(ambientLight);
-
-// Add directional light
-const directionalLight = new THREE.DirectionalLight(0xffeecc, 2);
-directionalLight.position.set(0, 10, 0);
-scene.add(directionalLight);
-
-// Add directional light helper
-const directionalLightHelper = new THREE.DirectionalLightHelper(
-    directionalLight,
-    5
-);
-scene.add(directionalLightHelper);
+let light = new Light(scene);
 
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(gridHelper);
 
-let t = 0;
 function animate() {
     requestAnimationFrame(animate);
-    t+=0.005;
-    directionalLight.position.x = 100 * Math.sin(t);
-    directionalLight.position.y = 100 * Math.cos(t);  
-    directionalLight.position.z = 50 * Math.cos(t);  
-
-
+    light.animate();
     renderer.render(scene, camera.perspectiveCamera);
 }
 
---
+++ Lab4/src/light.js
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+
+export default class Light {
+    constructor(scene, showHelper) {
+        scene.add(this.ambientLight);
+
+        this.directionalLight.position.set(0, 10, 0);
+        scene.add(this.directionalLight);
+
+        if (showHelper) scene.add(this.directionalLightHelper);
+    }
+    // Add ambient light
+    ambientLight = new THREE.AmbientLight(0xccccff, 0.2);
+
+    // Add directional light
+    directionalLight = new THREE.DirectionalLight(0xffeecc, 2);
+
+    // Add directional light helper
+    directionalLightHelper = new THREE.DirectionalLightHelper(
+        this.directionalLight,
+        5
+    );
+
+    t = 0;
+    animate() {
+        this.t += 0.005;
+        this.directionalLight.position.x = 1000 * Math.sin(this.t);
+        this.directionalLight.position.y = 1000 * Math.cos(this.t);
+        this.directionalLight.position.z = 500 * Math.cos(this.t);
+    }
+}
