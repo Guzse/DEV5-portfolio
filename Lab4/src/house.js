c9a0525a383113db384ef9ce364cd50ca970246a
GilianS <r0885351@student.thomasmore.be>
2022-10-14 11:54:32 +0200

    refactor: created array to loop objects easily

--- Lab4/src/house.js
+++ Lab4/src/house.js
@@ -71,45 +71,41 @@ export default class House {
         let geometryRoof = new THREE.BoxGeometry(8.5, 0.2, 5.5);
 
         // Create walls
-        let wall1 = new THREE.Mesh(geometryWall1, this.mBricks);
-        let wall2 = new THREE.Mesh(geometryWall2, this.mBricks);
-        let wall3 = new THREE.Mesh(geometryWall1, this.mBricks);
-        let wall4 = new THREE.Mesh(geometryWall2, this.mBricks);
-        wall1.position.set(...this.applyPosition(0, 2, 0));
-        wall2.position.set(...this.applyPosition(4, 2, 4));
-        wall3.position.set(...this.applyPosition(0, 2, 8));
-        wall4.position.set(...this.applyPosition(-4, 2, 4));
+        let object = [
+            new THREE.Mesh(geometryWall1, this.mBricks),
+            new THREE.Mesh(geometryWall2, this.mBricks),
+            new THREE.Mesh(geometryWall1, this.mBricks),
+            new THREE.Mesh(geometryWall2, this.mBricks),
+            new THREE.Mesh(geometryRoofSupport, this.mBricks),
+            new THREE.Mesh(geometryRoofSupport, this.mBricks),
+            new THREE.Mesh(geometryRoof, this.mRoof),
+            new THREE.Mesh(geometryRoof, this.mRoof),
+            new THREE.Mesh(new THREE.BoxGeometry(8, 2, 8), this.mConcrete),
+        ];
+        object[0].position.set(...this.applyPosition(0, 2, 0));
+        object[1].position.set(...this.applyPosition(4, 2, 4));
+        object[2].position.set(...this.applyPosition(0, 2, 8));
+        object[3].position.set(...this.applyPosition(-4, 2, 4));
 
         // Create roof support
-        let roofSupport1 = new THREE.Mesh(geometryRoofSupport, this.mBricks);
-        let roofSupport2 = new THREE.Mesh(geometryRoofSupport, this.mBricks);
-        roofSupport1.position.set(...this.applyPosition(-4, 4, -0.1));
-        roofSupport2.position.set(...this.applyPosition(-4, 4, 7.9));
+        object[4].position.set(...this.applyPosition(-4, 4, -0.1));
+        object[5].position.set(...this.applyPosition(-4, 4, 7.9));
 
         // Create roof
-        let roof1 = new THREE.Mesh(geometryRoof, this.mRoof);
-        roof1.position.set(...this.applyPosition(2.4, 4.8, 4));
-        roof1.rotateY(1.5708);
-        roof1.rotateX(0.48);
-        let roof2 = new THREE.Mesh(geometryRoof, this.mRoof);
-        roof2.position.set(...this.applyPosition(-2.4, 4.8, 4));
-        roof2.rotateY(-1.5708);
-        roof2.rotateX(0.48);
+        object[6].position.set(...this.applyPosition(2.4, 4.8, 4));
+        object[6].rotateY(1.5708);
+        object[6].rotateX(0.48);
+        object[7].position.set(...this.applyPosition(-2.4, 4.8, 4));
+        object[7].rotateY(-1.5708);
+        object[7].rotateX(0.48);
 
         // Create foundation
-        let foundation = new THREE.Mesh(new THREE.BoxGeometry(8, 2, 8), this.mConcrete);
-        foundation.position.set(...this.applyPosition(0, -1, 4));
+        object[8].position.set(...this.applyPosition(0, -1, 4));
 
-        this.scene.add(wall1);
-        this.scene.add(wall2);
-        this.scene.add(wall3);
-        this.scene.add(wall4);
-        this.scene.add(roofSupport1);
-        this.scene.add(roofSupport2);
-        this.scene.add(roof1);
-        this.scene.add(roof2);
-        this.scene.add(foundation);
-        
-        console.log("createStructure");
+        object.forEach(obj => {
+            obj.castShadow = true;
+            obj.receiveShadow = true;
+            this.scene.add(obj);
+        });
     }
 }
