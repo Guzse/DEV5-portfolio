e38a48d503e1e1e5cf91f2ed12bcadba6fc5361b
GilianS <r0885351@student.thomasmore.be>
2022-11-16 18:03:37 +0100

    feat: posting comments works

--- Lab5/src/components/CommentBox.vue
+++ Lab5/src/components/CommentBox.vue
@@ -1,38 +1,42 @@
 <template>
     <div class="comment-box--wrapper">
-        <textarea placeholder="Add a public comment..." v-model="comment" />
+        <textarea placeholder="Comment..." v-model="comment" />
         <button @click="addComment">Comment</button>
     </div>
 </template>
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
 
+let comment = ref('');
+
+const addComment = () => {
+    const apiUrl = 'https://lab5-p379.onrender.com/api/v1/messages/';
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user: 'Gilian',
+            text: comment.value,
+        })
+    })
+    .then(() => {
+        comment.value = '';
+    });
+}
 </script>
 <style lang="scss">
-    .comment-box--wrapper {
-        background-color: #f5f5f5;
+.comment-box--wrapper {
+    background-color: #f5f5f5;
+    padding: .5em;
+    display: flex;
+
+    textarea {
+        flex: 1;
+        border: none;
         padding: .5em;
-        display: flex;
-
-        textarea {
-            font-size: 1em;
-            resize: none;
-            border: none;
-            outline: none;
-            background-color: #ffffff;
-            padding: .5em;
-            border-radius: 1.2em;
-            margin-right: .5em;
-        }
-
-        button {
-            font-size: 1em;
-            border: none;
-            outline: none;
-            background-color: #ff00a2;
-            color: #ffffff;
-            padding: .5em 1em;
-            border-radius: 1.2em;
-        }
+        font-size: 1.2em;
     }
+}
 </style>
