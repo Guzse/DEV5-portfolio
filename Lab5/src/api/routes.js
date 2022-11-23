03e5e3fa7d8c6c6c276891bb916f12d8e296ad95
GilianS <r0885351@student.thomasmore.be>
2022-11-24 00:59:42 +0100

    feat: apparently render likes this?

--- Lab5/src/api/routes.js
+++ Lab5/src/api/routes.js
@@ -8,6 +8,9 @@ let { MessageRoutes } = require("./components/message/message.routes");
 function initiateRouter(app) {
     const prefix = "/api/v1";
 
+    app.get("/", (req, res) => {
+        res.sendStatus(200);
+    });
     app.get(prefix, (req, res) => res.send("PING"));
 
     app.use(`${prefix}/messages`, new MessageRoutes().router);
