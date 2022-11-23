3143d2c464bacb3c9868301e03f2a08d78a70b07
GilianS <r0885351@student.thomasmore.be>
2022-11-23 21:22:53 +0100

    feat: renamed certain functions for clarity

--- Lab5/server.js
+++ Lab5/server.js
@@ -1,11 +1,11 @@
 let express = require("express");
-const { initRestRoutes } = require("./src/api/routes");
+const { initiateRouter } = require("./src/api/routes");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-initRestRoutes(app);
+initiateRouter(app);
 
 app.listen(port, () =>
     console.log(`NodeJS Demo is listening on 127.0.0.1:${port}`)
--- Lab5/src/api/routes.js
+++ Lab5/src/api/routes.js
@@ -1,17 +1,17 @@
-let { MessageRoutes } = require('./components/message/routes');
+let { MessageRoutes } = require("./components/message/routes");
 /**
  * Init Express REST routes
  *
- * @param {Router} router
+ * @param {Express} app
  * @returns {void}
  */
- function initRestRoutes(router) {
-	const prefix = '/api/v1';
+function initiateRouter(app) {
+    const prefix = "/api/v1";
 
-	router.get(prefix, (req, res) => res.send('PING'));
-    
-    router.use(`${prefix}/messages`, new MessageRoutes().router);
+    app.get(prefix, (req, res) => res.send("PING"));
+
+    app.use(`${prefix}/messages`, new MessageRoutes().router);
 }
 module.exports = {
-    initRestRoutes
+    initiateRouter,
 };
