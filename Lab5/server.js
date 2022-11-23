8a10da224996eccbf96aaa679029e4a86ae59563
GilianS <r0885351@student.thomasmore.be>
2022-11-23 23:20:50 +0100

    feat: added mongodb and ENV variables

--- Lab5/server.js
+++ Lab5/server.js
@@ -1,4 +1,21 @@
-let express = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV == "production") {
+    console.log("%cProduction environment detected", "color: green");
+}
+else {
+    require("dotenv").config();
+    console.log("%cDevelopment environment detected", "color: green");
+}
+
+if (process.env.MONGO_URI) {
+
+}
+else {
+    console.error("ERROR: MONGO_URI not set");
+}
+
 const { initiateRouter } = require("./src/api/routes");
 const app = express();
 const port = 3000;
